681e591dde64a0ef49daf6961ce1f1f37abcebf3
Em Coraccio <emcoraccio@gmail.com>
2019-11-10 13:36:22 -0500

    Updates

---
+++ precache-manifest.db041845ce01b21b509257c0e719bdc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16cce16b4d2fe401e7059d7f2435b21",
+    "url": "/React-Memory-Game-TS/index.html"
+  },
+  {
+    "revision": "945bf473b8a4946f08b7",
+    "url": "/React-Memory-Game-TS/static/css/main.5c9f540d.chunk.css"
+  },
+  {
+    "revision": "43dd657182a36252c469",
+    "url": "/React-Memory-Game-TS/static/js/2.c5fbbe54.chunk.js"
+  },
+  {
+    "revision": "945bf473b8a4946f08b7",
+    "url": "/React-Memory-Game-TS/static/js/main.6e6fa5c7.chunk.js"
+  },
+  {
+    "revision": "89d7e7496439d1222bdc",
+    "url": "/React-Memory-Game-TS/static/js/runtime-main.b10ac9fa.js"
+  }
+]);
